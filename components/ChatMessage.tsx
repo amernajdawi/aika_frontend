d0ce611b9690abafc84c15557959938b474c5929
amernajdawi <amernajdawi8@gmail.com>
2025-10-12 14:08:37 +0200

    debug

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -152,6 +152,11 @@ export default function ChatMessage({ message, onCopy }: ChatMessageProps) {
     return parts.length > 0 ? parts : content;
   };
 
+  // Debug logging for relevant links
+  if (message.relevant_links && message.relevant_links.length > 0) {
+    console.log('🔍 Relevant Links Debug:', message.relevant_links);
+  }
+
   return (
     <div
       className={`flex animate-fadeIn ${
@@ -186,8 +191,6 @@ export default function ChatMessage({ message, onCopy }: ChatMessageProps) {
         {/* Relevant Links Section */}
         {message.relevant_links && message.relevant_links.length > 0 && (
           <div className="mt-3">
-            {/* Debug logging for relevant links */}
-            {console.log('🔍 Relevant Links Debug:', message.relevant_links)}
             <RelevantLinks links={message.relevant_links} />
           </div>
         )}
